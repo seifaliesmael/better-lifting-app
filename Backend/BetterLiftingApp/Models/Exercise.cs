9a7e53f396cddab02c3152e1fbedb7049dd80b48
Seif <seifali.esmael@gmail.com>
2026-06-21 01:11:29 +0100

    Initialised back-end in ASP.NET Core

---
+++ Backend/BetterLiftingApp/Models/Exercise.cs
@@ -0,0 +1,9 @@
+namespace BetterLiftingApp.Models
+{
+    public class Exercise
+    {
+        public int Id {get; set;}
+        public string ExerciseName {get; set;} = null!;
+        
+    }
+}
