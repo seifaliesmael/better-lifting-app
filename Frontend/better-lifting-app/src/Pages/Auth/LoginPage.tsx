5de1796374d17b7d5f77fc677f6627d8b56a01f5
Seif <seifali.esmael@gmail.com>
2026-07-25 12:29:50 +0400

    Endpoint fixes

--- Frontend/better-lifting-app/src/Pages/Auth/LoginPage.tsx
+++ Frontend/better-lifting-app/src/Pages/Auth/LoginPage.tsx
@@ -61,6 +61,7 @@ const LoginPage = ({updateView} : Props) => {
 
             <Button 
               variant="primary" 
+              disabled={!isPending}
               className="w-100 py-2 mb-4 fw-bold mt-4"
               onClick={() => handleLogin()}
             >
